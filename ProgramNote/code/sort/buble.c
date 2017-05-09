b268d8fa704a521fc5d61d7920a3fa184563a609
larryeppes <shuxuejia0805@126.com>
2017-05-09 23:00:21 +0800

    2017-05-09 23:00:21

---
+++ ProgramNote/code/sort/buble.c
@@ -0,0 +1,6 @@
+for(i=n; i>1; i--) {
+  for(j=0; j<i-1; j++) {
+    if(|$a_j<a_{j+1}$|)
+      |$a_j\leftrightarrow a_{j+1}$|
+  }
+}
---
+++ ProgramNote/code/sort/insert.c
@@ -0,0 +1,6 @@
+for(i=1; i<n; i++) {
+  for(j=0; j<i; j++) {
+    if(|$a_i<a_j$|)
+      |$a_j$|.prepend(|$a_i$|)
+  }
+}
